ccd3a71e843aeeb7cd2fbc68aa8e229738203ced
psatomas <psatomas@gmail.com>
2025-05-25 15:14:35 -0300

    feat: add create_a_struct_instance.rs

---
+++ src/bin/create_a_struct_instance.rs
@@ -0,0 +1,13 @@
+fn main() {
+    struct Coffee {
+        price: f64,
+        name: String,
+        is_hot: bool,
+    }
+
+    let mocha = Coffee {
+        name: String::from("Mocha"),
+        price: 4.99,
+        is_hot: false,
+    }
+}
