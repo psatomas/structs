6a81f64503fd825f4f86ed62ddd7d95a36dda122
psatomas <psatomas@gmail.com>
2025-05-30 08:38:27 -0300

    feat: add struct_update_syntax.rs

---
+++ src/bin/struct_update_syntax.rs
@@ -0,0 +1,24 @@
+struct Coffee {
+    price: f64,
+    name: String,
+    is_hot: bool,
+}
+fn main() {
+    let mocha: Coffee = make_coffee(String::from("Mocha"), 4.99, true);
+
+    let caramel_macchiato = Coffee {
+        name: mocha.name.clone(),
+        ..mocha
+    };
+
+    println!("{}", caramel_macchiato.name);
+    println!("{}", mocha.name);
+}
+
+    fn make_coffee(name: String, price: f64, is_hot: bool) -> Coffee {
+    Coffee {
+        name,
+        price,
+        is_hot,
+    }
+}
