4dae5ddb132af1f5c934cf69df867ea4aa3b691f
psatomas <psatomas@gmail.com>
2025-05-31 18:45:09 -0300

    feat: add passing_structs_into_a_function.rs

---
+++ src/bin/passing_structs_into_a_function.rs
@@ -0,0 +1,25 @@
+struct Coffee {
+    price: f64,
+    name: String,
+    is_hot: bool,
+}
+fn main() {
+    let mut mocha: Coffee = make_coffee(String::from("Mocha"), 4.99, true);
+    drink_coffee(&mut mocha);
+
+    println!("{}", mocha.price)
+}
+
+    fn make_coffee(name: String, price: f64, is_hot: bool) -> Coffee {
+    Coffee {
+        name,
+        price,
+        is_hot,
+    }
+}
+
+fn drink_coffee(coffee: &mut Coffee) {
+    println!("Drinking my delicious {}", coffee.name);
+    coffee.is_hot = false; 
+    coffee.price = 10.99;
+}
